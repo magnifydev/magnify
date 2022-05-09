28b9efaca234c3a343627824a69d42d060907f86
Ascent817 <vapradeep817@gmail.com>
2022-05-09 17:52:52 -0500

    Started routing development

--- src/App.js
+++ src/App.js
@@ -29,6 +29,33 @@ function App(props) {
     //#endregion
 
     const [user, setUser] = useState(null);
+    const dummy = {
+        "uid": "xcZK6Rp2THXZ520ncMk33KrbvbT2",
+        "email": "vapradeep817@gmail.com",
+        "emailVerified": true,
+        "displayName": "Ascent",
+        "isAnonymous": false,
+        "photoURL": "https://lh3.googleusercontent.com/a-/AOh14GgpyS6fYl7CyUzAx_Cy7TJos-ppsuys-yTCZEulkQ=s96-c",
+        "providerData": [
+            {
+                "providerId": "google.com",
+                "uid": "115514179979743638838",
+                "displayName": "Ascent",
+                "email": "vapradeep817@gmail.com",
+                "phoneNumber": null,
+                "photoURL": "https://lh3.googleusercontent.com/a-/AOh14GgpyS6fYl7CyUzAx_Cy7TJos-ppsuys-yTCZEulkQ=s96-c"
+            }
+        ],
+        "stsTokenManager": {
+            "refreshToken": "AIwUaOnFTUUqVos2-Z6oly0QqWjl1PAu3SBdbKt6VP_qinjCl0JG0MdrDFrvS8xlTwxlS0NbCj2uKpEYyCdtdhUvHRquSDPTS8Ty42-fsHSh5HeR6OtFTD0CiM1vXMI435TZoN_nk20AMPiJazfhxWJ2QPge1IToeGbHHNKYMpfRLJ4zBBmkJM2fyT3dygOa_GEr76-wETO9bv_brmA6xaw6BMoSBRyAg5UBUj528_nEKPGMaw0blNHyzH9IuGWgRKTBtsj6b98m67hd4bXC8Ohwy2QwfkjYCmBdNkjdCvpEfmzM6lWNANi-h0RaPsEMq--R9-rnjn6hX6TwiU_vBpf1SuSDyIe0KgoFjYlPpGVXAcmxlcZO82zNozkgrhDC3RGtfBzf7MvBLXaX24YAkmyDWdf_57m20kSfTxjIVwm6FhlLg9Z6hQs",
+            "accessToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6ImVmMzAxNjFhOWMyZGI3ODA5ZjQ1MTNiYjRlZDA4NzNmNDczMmY3MjEiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiQXNjZW50IiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hLS9BT2gxNEdncHlTNmZZbDdDeVV6QXhfQ3k3VEpvcy1wcHN1eXMteVRDWkV1bGtRPXM5Ni1jIiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2NvdXJzZWluc3BlY3RvciIsImF1ZCI6ImNvdXJzZWluc3BlY3RvciIsImF1dGhfdGltZSI6MTY1MTk2MzM2MSwidXNlcl9pZCI6InhjWks2UnAyVEhYWjUyMG5jTWszM0tyYnZiVDIiLCJzdWIiOiJ4Y1pLNlJwMlRIWFo1MjBuY01rMzNLcmJ2YlQyIiwiaWF0IjoxNjUxOTYzMzYxLCJleHAiOjE2NTE5NjY5NjEsImVtYWlsIjoidmFwcmFkZWVwODE3QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7Imdvb2dsZS5jb20iOlsiMTE1NTE0MTc5OTc5NzQzNjM4ODM4Il0sImVtYWlsIjpbInZhcHJhZGVlcDgxN0BnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.QUi4KYwsjKd-W7U2mBRSqbuqnpkyWr08mNNN4SN34OEsQyEyj90C-8DS8Ip41GjBqoX-SyRINRH4xtI-3_KbhLfRHuujxLkgomXkX6HBTwPd11Fh9ENsart7BYXvCZONows_nK-KUU4cg-Edbbrdgh5W8u8UK5xWuDCeXb83JYAg26Znt1vgFk-4J6LZb4Zm0R5MrJs_Cr60-E2cP5PXhSiUNJY2t8nx_BLpbMvY0Ol888R_xu_O0XY-1s2a9MQumY2kY7o3UU11ngtW7FF8LaqdnDhUKkeAJiAwAkGcb7jhs1fg_AoE1IROf5CLGU6m6eTWLn1dKE2Q29DGXaOqtg",
+            "expirationTime": 1651966961400
+        },
+        "createdAt": "1649263590959",
+        "lastLoginAt": "1651963361860",
+        "apiKey": "AIzaSyB7xPx53G2Q39jxMghxSN2P1vaf0YjukwE",
+        "appName": "[DEFAULT]"
+    }
 
     // When the user clicks on the button, scroll to the top of the document
     function topFunction() {
@@ -51,9 +78,11 @@ function App(props) {
         .then((result) => {
             // The signed-in user info.
             if (result.user != null) {
-                console.log(result.user);
-                setUser(result.user);
+                console.log(`Setting user to ${dummy}`);
+                setUser(dummy)
             }
+
+            console.log(`User is ${user}`);
 
             dbRef.get().then((snapshot) => {
                 if (snapshot.exists()) {
@@ -64,7 +93,7 @@ function App(props) {
 
                         try {
                             Object.keys(authdata).forEach(key => {
-                                if (user._delegate.email == authdata[key].email) {
+                                if (user.email == authdata[key].email) {
                                     authlevel = authdata[key].level;
                                 }
                             });
@@ -194,10 +223,11 @@ function App(props) {
                     </button>
 
                     <div className="user">
-                        <img id="user-img" src={user ? user._delegate.photoURL : userdefault}></img>
+                        <img id="user-img" src={user ? user.photoURL : userdefault}></img>
                     </div>
 
                     <button onClick={() => console.log(user)}>Bleh</button>
+                    <button onClick={() => setUser(dummy)}>Bleb</button>
                 </div>
                 <div className="routeFrame"></div>
             </div>
