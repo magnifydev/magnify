08866524abbd266312cb69ab31b09b5266f270b1
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2024-01-25 13:43:42 -0600

    Bump version for release 2024-2025

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,5 @@
 export const links = {
   github: 'https://github.com/magnifydev/magnify',
   programOfStudiesPDF:
-    'https://linnmark12iaus.b-cdn.net/wp-content/uploads/2016/11/2023-2024-LMHS-Program-of-Studies-FINAL1.pdf',
+    'https://linnmark12iaus.b-cdn.net/wp-content/uploads/2016/11/2024-2025-LMHS-Program-of-Studies-FINAL1.pdf',
 };
